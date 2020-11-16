a5fc66d43c42d0596239cdd6b41c14d31eb73868
shaneh22 <shaneh22@live.unc.edu>
2020-11-15 23:21:59 -0500

    AsteroidsGame

---
+++ Games/Asteroids/game.js
@@ -0,0 +1,54 @@
+const asteroids = $('#asteroids');
+
+document.body.style.cursor = 'Asteroid.png';
+
+let speed = 1500;
+let count = 0;
+
+$(document).ready(() => {
+    asteroids.on('click', () => {
+        count++;
+        $('#count').html(count);
+        $('.asteroid').remove();
+    })
+})
+
+const generateAsteroid = (num) => {
+    let asteroid = $(`<div class = "asteroid"></div>`);
+    let random = Math.random();
+    if (random > .5) {
+        if(random > .75){
+            asteroid.addClass('right');
+        }
+        else{
+            asteroid.addClass('left');
+        }
+        speed = 1500;
+        if (random > .8) {
+            asteroid.css('top', '0px');
+        }
+        else if (random > .6) {
+            asteroid.css('top', '225px');
+        }
+    }
+    else {
+        if(random > .25){
+            asteroid.addClass('down');
+        }
+        else{
+            asteroid.addClass('up');
+        }
+        asteroid.css('left', `${random * 1400}px`);
+        speed = 1250;
+    }
+    let randomSize = 115 + Math.random() * 25;
+    asteroid.css({ "width": `${randomSize}px`, "height": `${randomSize}px`, "background-size": `${randomSize}px ${randomSize}px` });
+    let randomRot = 360 * Math.random();
+    asteroid.css('transform', `rotate(${randomRot}deg)`);
+    asteroids.append(asteroid);
+    setTimeout(() => {
+        asteroid.remove();
+    }, speed);
+};
+
+let runGame = setInterval(generateAsteroid, speed + 50);
---
+++ Games/DinosaurRemaster/game.js
@@ -0,0 +1,101 @@
+const ship = $('#spaceship');
+const asteroids = $('#asteroids');
+
+let timeInterval = 1000;
+let speed = 950;
+
+let animate = (animation) => {
+    if (document.getElementById('spaceship').classList.length === 0) {
+        ship.addClass(animation);
+        setTimeout(() => {
+            ship.removeClass(animation);
+        }, 500);
+    }
+}
+
+const generateAsteroid = (num)=>{
+    let asteroid = $(`<div class = "asteroid"></div>`);
+    let randomHeight = Math.random();
+    if(randomHeight > .8){
+        asteroid.css('top','-40px');
+    }
+    else if(randomHeight > .6){
+        asteroid.css('top','225px');
+    }
+    let randomSize = 100 + Math.random() * 25;
+    asteroid.css({"width":`${randomSize}px`, "height":`${randomSize}px`,"background-size":`${randomSize}px ${randomSize}px`});
+    let randomRot = 360 * Math.random();
+    asteroid.css('transform',`rotate(${randomRot}deg)`);
+    asteroids.append(asteroid);
+    setTimeout(()=>{
+        asteroid.remove();
+    }, speed);
+
+    if(num === 2){
+        let newAsteroid = $(`<div class = "asteroid2"></div>`);
+        if(randomHeight > .8){
+            newAsteroid.css('top','285px');
+        }
+        else if(randomHeight > .6){
+            newAsteroid.css('top', '140px');
+        }
+        else {
+            newAsteroid.css('top', '15px');
+        }
+        randomSize = 100 + Math.random() * 15;
+        newAsteroid.css({"width":`${randomSize}px`, "height":`${randomSize}px`,"background-size":`${randomSize}px ${randomSize}px`});
+        randomRot = 360 * Math.random();
+        newAsteroid.css('transform',`rotate(${randomRot}deg)`);
+        asteroids.append(newAsteroid);
+        setTimeout(()=>{
+            newAsteroid.remove();
+        }, speed);
+    }
+};
+
+let generateAsteroids = () =>{
+    asteroids.empty();
+    let numAsteroids = Math.random();
+    if(numAsteroids > .7){
+        generateAsteroid(2);
+    }
+    else{
+        generateAsteroid(1);
+    }
+}
+
+let runGame = setInterval(generateAsteroids, timeInterval);
+
+let isAlive = setInterval(()=>{
+    let asteroid = $('.asteroid');
+    let shipTop = parseInt($('#spaceship').css('top'));
+    let asteroidLeft = parseInt(asteroid.css('left'));
+    let asteroidTop = parseInt(asteroid.css('top'));
+    let asteroidBottom = parseInt(asteroid.css('top')) + parseInt(asteroid.css('height'));
+
+    let asteroid2 = $('.asteroid2');
+    let asteroid2collision = false;
+    if(asteroid2 != null){
+        let asteroid2Left = parseInt(asteroid2.css('left'));
+        let asteroid2Top = parseInt(asteroid2.css('top'));
+        let asteroid2Bottom = parseInt(asteroid2.css('top')) + parseInt(asteroid2.css('height'));
+        asteroid2collision = asteroid2Left < 125 && asteroid2Left > 10 && shipTop < asteroid2Bottom && shipTop + 50 > asteroid2Top;
+    }
+
+    if((asteroidLeft < 125 && asteroidLeft > 10 && shipTop < asteroidBottom && shipTop + 50 > asteroidTop) || (asteroid2collision)){
+        alert('You died !');
+        clearInterval(runGame);
+        setTimeout(2000);
+        runGame = setInterval(generateAsteroids, timeInterval);
+    }
+}, 10);
+
+document.addEventListener('keydown', (evt) => {
+    if(evt.key === 'ArrowDown'){
+        animate('down');
+    }
+    else if (evt.key === 'ArrowUp'){
+        animate('up');
+    }
+});
+
