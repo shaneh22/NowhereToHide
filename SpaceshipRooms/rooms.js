6993db1ddd36dceede68dddd0f557955747232e3
shaneh22 <shaneh22@live.unc.edu>
2020-11-19 00:18:41 -0500

    SpaceshipRooms

--- SpaceshipRooms/rooms.js
+++ SpaceshipRooms/rooms.js
@@ -2,11 +2,20 @@ let display = $('#display');
 let columns = $('<div class = "columns is-multiline justify-center"></div>');
 
 let createRoom = (name) => {
-    let box = $(`<div class = "box"></box>`);
+    let box = $(`<div class = "box has-text-centered"></box>`);
     box.css('height', '250px');
     box.css('margin-top', '40px');
     let roomName = $(`<h1 class= "hero is-centered">${name}</name>`);
     box.append(roomName);
+    //This is where we would display how many players are in the room
+    box.append(`<h4 style = "font-size: 75px" class= "mb-4" id="${name}">0</h6>`);
+    let enterButton = $(`<button class = "button is-dark enter">Enter</button>`);
+    enterButton.on('click', ()=>{
+        //send choice to backend and update the number with new total of players from backend;
+        $('.enter').remove();
+    });
+    box.append(enterButton);
+    
     return box;
 }
 
@@ -16,8 +25,14 @@ let createColumn = (name1, name2) =>{
     column.append(createRoom(name2));
     return column;
 }
-
-columns.append(createColumn('Room1','Room2'));
-columns.append(createColumn('Room3', 'Room4'));
-columns.append(createColumn('Room5','Room6'));
+//I'm thinking :
+//Observatory: Memory
+//Cafeteria: Snake Game
+//Cockpit: "DinosaurRemaster" - Piloting the Ship
+//Defense: Asteroids
+//Electrical: Math Trivia
+//Engine: 2048 
+columns.append(createColumn('Electrical','Engine'));
+columns.append(createColumn('Cafeteria', 'Observatory'));
+columns.append(createColumn('Cockpit','Defense'));
 display.append(columns);
