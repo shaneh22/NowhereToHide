e6de004ae95c273e38b1de483ad87c9d23b17507
rupshar <rupshar@live.unc.edu>
2020-11-18 19:14:27 -0800

    Made some CSS changes to 2048 and Trivia

---
+++ Games/TriviaGame/quiz.js
@@ -0,0 +1,515 @@
+let quizQuestions = [
+    {
+        question: "What is 2 + 2?",
+        answers: {
+            A: '3',
+            B: '4',
+            C: '5',
+            D: '6',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 3 + 2?",
+        answers: {
+            A: '3',
+            B: '4',
+            C: '5',
+            D: '6',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 3 + 3?",
+        answers: {
+            A: '3',
+            B: '4',
+            C: '5',
+            D: '6',
+        },
+        correct: 'D',
+    },
+    {
+        question: "What is 1 + 2?",
+        answers: {
+            A: '3',
+            B: '4',
+            C: '5',
+            D: '6',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 5 + 6?",
+        answers: {
+            A: '13',
+            B: '14',
+            C: '11',
+            D: '12',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 8 + 7?",
+        answers: {
+            A: '13',
+            B: '14',
+            C: '15',
+            D: '16',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 0 + 2?",
+        answers: {
+            A: '2',
+            B: '1',
+            C: '0',
+            D: '9',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 9 + 4?",
+        answers: {
+            A: '13',
+            B: '14',
+            C: '15',
+            D: '16',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 4 + 3?",
+        answers: {
+            A: '7',
+            B: '4',
+            C: '9',
+            D: '12',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 9 + 9?",
+        answers: {
+            A: '18',
+            B: '12',
+            C: '19',
+            D: '61',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 6 - 2?",
+        answers: {
+            A: '3',
+            B: '4',
+            C: '5',
+            D: '6',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 8 - 1?",
+        answers: {
+            A: '7',
+            B: '3',
+            C: '1',
+            D: '12',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 6 - 8?",
+        answers: {
+            A: '-1',
+            B: '0',
+            C: '4',
+            D: '-2',
+        },
+        correct: 'D',
+    },
+    {
+        question: "What is 12 - 12?",
+        answers: {
+            A: '0',
+            B: '4',
+            C: '8',
+            D: '12',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 5 - 6?",
+        answers: {
+            A: '-1',
+            B: '-2',
+            C: '-3',
+            D: '-4',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 40 - 15?",
+        answers: {
+            A: '25',
+            B: '15',
+            C: '35',
+            D: '16',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 0 - 9?",
+        answers: {
+            A: '-8',
+            B: '-9',
+            C: '1',
+            D: '-2',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 9 - 4?",
+        answers: {
+            A: '4',
+            B: '5',
+            C: '9',
+            D: '10',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 4 - 3?",
+        answers: {
+            A: '4',
+            B: '1',
+            C: '2',
+            D: '0',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 2 - 8?",
+        answers: {
+            A: '5',
+            B: '-8',
+            C: '-2',
+            D: '-6',
+        },
+        correct: 'D',
+    },
+    {
+        question: "What is 8 * 4?",
+        answers: {
+            A: '32',
+            B: '42',
+            C: '52',
+            D: '62',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 8 * 0?",
+        answers: {
+            A: '8',
+            B: '1',
+            C: '0',
+            D: '2',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 11 * 11?",
+        answers: {
+            A: '121',
+            B: '100',
+            C: '99',
+            D: '89',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 25 * 25?",
+        answers: {
+            A: '300',
+            B: '485',
+            C: '625',
+            D: '1000',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 10 * 10?",
+        answers: {
+            A: '100',
+            B: '20',
+            C: '50',
+            D: '25',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 38 * 2?",
+        answers: {
+            A: '76',
+            B: '75',
+            C: '85',
+            D: '86',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 6 * 6?",
+        answers: {
+            A: '26',
+            B: '36',
+            C: '1',
+            D: '12',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 9 * 4?",
+        answers: {
+            A: '45',
+            B: '18',
+            C: '36',
+            D: '10',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 4 * 3?",
+        answers: {
+            A: '12',
+            B: '24',
+            C: '7',
+            D: '8',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 12 * 12?",
+        answers: {
+            A: '144',
+            B: '132',
+            C: '156',
+            D: '169',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 8 / 4?",
+        answers: {
+            A: '4',
+            B: '16',
+            C: '2',
+            D: '12',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 8 / 0?",
+        answers: {
+            A: 'infinity',
+            B: 'undefined',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 0 / 11?",
+        answers: {
+            A: '11',
+            B: '12',
+            C: '0',
+            D: '2',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 169 / 13?",
+        answers: {
+            A: '13',
+            B: '14',
+            C: '15',
+            D: '18',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 10 / 10?",
+        answers: {
+            A: '1',
+            B: '2',
+            C: '10',
+            D: '20',
+        },
+        correct: 'A',
+    },
+    {
+        question: "What is 38 / 2?",
+        answers: {
+            A: '76',
+            B: '18',
+            C: '85',
+            D: '28',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 12 / 3?",
+        answers: {
+            A: '6',
+            B: '12',
+            C: '4',
+            D: '9',
+        },
+        correct: 'C',
+    },
+    {
+        question: "What is 9 / 2?",
+        answers: {
+            A: '13',
+            B: '4.5',
+            C: '2.25',
+            D: '11',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 9 / 3?",
+        answers: {
+            A: '4',
+            B: '3',
+            C: '1',
+            D: '2',
+        },
+        correct: 'B',
+    },
+    {
+        question: "What is 576 / 24?",
+        answers: {
+            A: '26',
+            B: '24',
+            C: '34',
+            D: '29',
+        },
+        correct: 'B',
+    },
+]
+
+let randQuiz = questionSelector();
+
+$(function() {
+    renderQuiz();
+
+    let quiz = document.getElementById('customQuiz')
+
+    renderQuestions(randQuiz, quiz);
+
+    const $root = $('#root')
+
+    $root.on('click', '#submit', getAnswers)
+});
+
+function questionSelector() {
+    let ret_arr = new Array();
+
+    for(let i = 0; i < 8; i++) {
+        let randomNum = Math.floor(Math.random() * 40);
+        console.log(randomNum)
+        ret_arr.push(quizQuestions[randomNum])
+    }
+
+    return ret_arr
+}
+
+function renderQuiz() {
+    const $root = $('#root');
+
+    $root.append(`<div id="customQuiz"></div>
+                    <button id="submit">Submit Answers</button>
+                    <div id="results"></div>`)
+}
+
+function getAnswers() {
+    let quiz = document.getElementById('customQuiz')
+    let results = document.getElementById('results');
+    showResults(randQuiz, quiz, results);
+}
+
+function showResults(questions, quiz, result){
+    let answerContainers = quiz.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let userAnswer = '';
+    let numCorrect = 0;
+    
+    // for each question...
+    for(let i=0; i<questions.length; i++){
+
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correct){
+            // add to the number of correct answers
+            numCorrect++;
+            
+            // color the answers green
+            answerContainers[i].style.color = 'green';
+        }
+        // if answer is wrong or blank
+        else{
+            // color the answers red
+            answerContainers[i].style.color = 'red';
+        }
+
+        if(numCorrect < questions.length) {
+            result.style.color = 'red';
+            result.innerHTML = "You lose!"
+        } else {
+            result.style.color = 'green';
+            result.innerHTML = "YOU WIN!!!"
+        }
+    }
+}
+
+function renderQuestions(questions, quiz) {
+    let question = [];
+    let answers;
+
+    for(let i=0; i<questions.length; i++){
+        
+        answers = [];
+
+        for(letter in questions[i].answers){
+
+            answers.push(
+                '<label>'
+                    + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                    + `<strong>`+ letter + `</strong>` + ': '
+                    + questions[i].answers[letter]
+                + '</label>'
+                // '<input type="radio" name="question' + i + '"id="radio' + i +  '" value="' + letter + '">'
+            );
+
+            // answers.push(
+            //     '<label for="radio' + i + '">'  + letter + ': ' + questions[i].answers[letter] + '</label>'
+            // )
+
+        }
+
+        question.push(
+            '<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">' + answers.join('') + '</div>'
+        );
+
+        $('label:has(input="type=radio")').addClass('newColor');
+
+    }
+    
+    $('#customQuiz').append(question.join(''))
+
+}
